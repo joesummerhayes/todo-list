a4d92d223f206846153b72a90a8444cedf3479e0
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-04-03 13:34:58 +0100

    second change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Todolist from './components/TodoList';
 import TodoInput from './components/TodoInput';
 import TodoItem from './components/TodoItem';
 
-//showing change a';slda;'sldas
-
+//this is a new changee
 class App extends Component {
   render() {
     return (
