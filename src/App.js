2942eff9aaa86c31473a86e10c270f7ad219ab60
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-04-04 19:56:26 +0100

    added helper functions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,62 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Todolist from './components/TodoList';
 import TodoInput from './components/TodoInput';
-import TodoItem from './components/TodoItem';
+
 
 //this is a new changee
 class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid(),
+    item: '',
+    editItem: false,
+
+  };
+
+  handleChange = (e) => {
+    console.log('handle change')
+  };
+
+  handleSubmit = (e) => {
+    console.log('handle Submit')
+  };
+
+  clearList = () => {
+    console.log('clear list')
+  };
+
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`)
+  };
+
+  handleDelete = (id) => {
+    console.log(`handle edit ${id}`)
+  }
+
+
   render() {
+
+
     return (
       <div className="container">
-        <TodoInput />
-        <Todolist />
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-centre">
+              to-do-input
+          </h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <Todolist />
+          </div>
+        </div>
       </div>
+
+
+
     )
   }
 }
